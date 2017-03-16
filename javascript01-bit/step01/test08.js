33aa8f6739f93b5c6472539b26eb971b544cf35c
eomjinyoung <jinyoung.eom@gmail.com>
2017-03-16 10:19:58 +0900

    .

---
+++ javascript01-bit/step01/test08.js
@@ -0,0 +1,22 @@
+/* 주제: 변수 선언과 값 할당 II
+*/
+"use strict"
+var name = "홍길동";
+var tel = "1111-2222";
+var kor = 100;
+var eng = 100;
+var math = 100;
+
+console.log(name, tel, kor, eng, math);
+
+var name2 = "임꺽정",
+    tel2 = "2222-2222",
+    kor2 = 90,
+    eng2 = 90,
+    math2 = 90;
+console.log(name2, tel2, kor2, eng2, math2);
+
+
+
+
+//
