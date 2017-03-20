74f4a06ffe2b61d6edb9d84b2b952cb288ab8c87
eomjinyoung <jinyoung.eom@gmail.com>
2017-03-20 11:24:54 +0900

    .

--- javascript01-bit/step03/test05.js
+++ javascript01-bit/step03/test05.js
@@ -50,7 +50,22 @@ else // <-- age >= 8, if ~ else ~ 는 한 문장이기 때문에 블록을 생�
         console.log("노인");
 console.log("----------------------");
 
-
+// 실무에서는 if ~ else ~ 문장을 보기 좋게 정렬한다.
+// 주의!
+// - if ~ else if ~ else if ~ else 라는 문법은 없다!
+// - 그냥 if ~ else 코드를 정돈한 것이다.
+age = 65;
+if (age < 8)
+  console.log("유아");
+else if (age >= 8 && age < 14)
+  console.log("어린이");
+else if (age >= 14 && age < 20)
+  console.log("청소년");
+else if (age >= 20 && age < 65)
+  console.log("성인");
+else
+  console.log("노인");
+console.log("----------------------");
 
 
 //
---
+++ javascript01-bit/step03/test07.js
@@ -0,0 +1,23 @@
+/* 주제: 흐름 제어문 - switch ~ case
+- 복잡한 조건에 따라 실행 흐름을 제어할 때는 if를 사용한다.
+- 조건이 단순할 때는 switch 문을 사용하는 것이 편리하다.
+- 문법
+switch(식) {
+case 값1:
+  식1;
+  식2;
+  ...
+  break;
+case 값2:
+  ...
+  break;
+default:
+  ...
+}
+*/
+"use strict"
+
+var age;
+
+
+//
