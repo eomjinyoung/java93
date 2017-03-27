c8ef9298b2fa81cb98ddf26b7d73f2b4430b8b81
eomjinyoung <jinyoung.eom@gmail.com>
2017-03-27 11:51:44 +0900

    .

--- javascript01-bit/step06/test07.js
+++ javascript01-bit/step06/test07.js
@@ -1,27 +1,32 @@
-/* 주제: 모듈 만들기
-- 함수들이나 객체를 별도의 파일로 분리하여 관리하기
-- 즉 몇 백개, 몇 천개, 몇 만개의 함수나 객체를 한 개의 파일에서 관리할 수 없다.
-- 함수나 객체들을 역할이나 기능에 따라 적절히 분류하여
-  여러 개의 파일에 분산해 두는 것이 유지보수에 좋다.
-  이렇게 분리된 함수나 객체들의 묶음을 "모듈(module)"이라 부른다.
-1) 모듈 정의
-   => test06_m1.js
-   => test06_m2.js
-2) 모듈 사용
-   => require('모듈명 또는 모듈 경로')
+/* 주제: exports 를 사용자가 다른 객체로 대체하기
+- 기존에 존재하는 exports 객체에 뭔가를 담아서 리턴할 수 있지만,
+  아예 exports 객체를 다른 객체로 대체해서 리턴할 수 있다.
 */
 "use strict"
 
-// require(모듈 경로)
-// => module 객체에 보관된 exports 객체를 리턴한다.
-var m1 = require('./test06_m1.js')
+var m1 = require('./test07_m1.js')
 
-// m1 <= module.exports 객체를 가리킨다.
 console.log(m1.plus(10, 20))
 console.log(m1.minus(10, 20))
 console.log(m1.multiple(10, 20))
-// divide() 함수는 exports 객체에 등록되지 않았기 때문에 호출할 수 없다.
-//console.log(m1.divide(10, 20))
+console.log(m1.divide(10, 20))
+
+// require()가 리턴한 객체에서 plus()와 minus() 함수 주소만 꺼내 변수에 저장한다.
+var {plus, minus} = require('./test07_m1.js')
+/*
+var plus = require('./test07_m1.js').plus
+var minus = require('./test07_m1.js').minus
+*/
+/*
+var m = require('./test07_m1.js')
+var plus = m.plus
+var minus = m.minus
+*/
+console.log(plus(10, 20))
+console.log(minus(10, 20))
+
+
+
 
 
 //
--- javascript01-bit/step06/test07_m1.js
+++ javascript01-bit/step06/test07_m1.js
@@ -1,22 +1,15 @@
-/* 주제: 모듈 정의
-- 모듈로 관리할 함수나 객체를 정의한 후
-  module.exports 라는 객체에 그 주소를 저장한다.
+/* 주제: exports 객체를 다른 객체로 대체하기
 */
 "use strict"
 
-function plus(a, b) {return a + b}
-function minus(a, b) {return a - b}
-function multiple(a, b) {return a * b}
-function divide(a, b) {return a / b}
+// 기존에 module에 들어있던 exports 객체를 우리가 만든 객체로 대체한다.
+module.exports = {
+  plus(a, b) {return a + b},
+  minus(a, b) {return a - b},
+  multiple(a, b) {return a * b},
+  divide(a, b) {return a / b}
+}
 
-// module.exports.모듈을사용하는측에서호출할이름 = 함수주소
-// module 객체에 exports 라는 객체가 미리 등록되어 있다.
-// 그래서 미리 등록된 exports 라는 객체에 함수나 또 다른 객체를 저장하는 것이다.
-module.exports.plus = plus;
-
-// module.exports 에서 module을 생략할 수 있다.
-exports.minus = minus;
-exports.multiple = multiple;
 
 
 
