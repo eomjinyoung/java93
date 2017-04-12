da58526fcac53cfef749e6ecd299bb4a0349a9f4
eomjinyoung <jinyoung.eom@gmail.com>
2017-04-12 15:51:42 +0900

    .

--- javascript01-bit/step08/test10.js
+++ javascript01-bit/step08/test10.js
@@ -37,7 +37,6 @@ function bit(value) {
     for (var e of this) {
       e.innerHTML = content
     }
-
     return this
   }
 
@@ -50,9 +49,22 @@ function bit(value) {
   }
 
   // 4) 배열 안에 있는 태그의 속성 값을 다루는 함수
-  el.attr = function(name) {
-    // 값을 꺼낼 때는 배열에서 0번 방에 들어 있는 태그의 속성 값만 꺼내자!
-    return this[0].getAttribute(name)
+  // => 개발자가 태그에 추가한 속성을 다룬다.
+  // => 속성을 값을 꺼낼 때는 getAttribute()를 사용하고,
+  //    속성의 값을 넣을 때는 setAttribute()를 사용한다.
+  // => 태그에 원래 있던 속성에 대해서도 사용할 수 있는데,
+  //    "checked"와 같은 일부 속성은 적용할 수 없다.
+  el.attr = function(name, value) {
+    if (arguments.length < 2) { // 특정 이름을 가진 속성의 값을 꺼낸다.
+      // 값을 꺼낼 때는 배열에서 0번 방에 들어 있는 태그의 속성 값만 꺼내자!
+      return this[0].getAttribute(name)
+
+    } else { // 특정 이름을 가진 속성의 값을 value로 설정한다.
+      for (var e of this) {
+        e.setAttribute(name, value)
+      }
+      return this
+    }
   }
 
   // 5) 배열 안에 있는 태그를 부모 태그에 추가시키는 함수
@@ -63,6 +75,45 @@ function bit(value) {
       }
     }
     return this;
+  }
+
+  // 6) 배열 안에 있는 각각의 태그에 대해 CSS 스타일을 다루는 함수
+  el.css = function(name, value) {
+    for (var e of this) {
+      e.style[name] = value
+    }
+    return this
+  }
+
+  // 7) 배열 안에 있는 태그의 콘텐츠를 설정하는 함수(textContent 속성)
+  el.text = function(content) {
+    for (var e of this) {
+      e.textContent = content
+    }
+    return this
+  }
+
+  // 8) 배열 안에 있는 폼 항목 태그의 콘텐츠를 설정하는 함수(value 속성)
+  el.val = function(value) {
+    for (var e of this) {
+      e.value = value
+    }
+    return this
+  }
+
+  // 9) 배열 안에 있는 태그의 프로퍼티 값(?)을 다루는 함수
+  //    => 개발자가 태그에 추가한 속성이 아닌 원래 있던 속성의 값을 다루는 함수
+  //    => getAttribute()/setAttribute() 대신 "태그명.속성명", "태그명.속성명 = 값"으로 다룬다.
+  el.prop = function(name, value) {
+    if (arguments.length < 2) { // 특정 이름을 가진 프로퍼티 값을 꺼낸다.
+      return this[0][name]
+
+    } else { // 특정 이름을 가진 속성의 값을 value로 설정한다.
+      for (var e of this) {
+        e[name] = value
+      }
+      return this
+    }
   }
 
 
@@ -101,6 +152,13 @@ bit.ajax = function(url, settings) {
   }
   xhr.open(settings.method, url, true)
   xhr.send()
+}
+
+bit.getJSON = function(url, success) {
+  bit.ajax(url, {
+    dataType: 'json',
+    success: success
+  })
 }
 
 
