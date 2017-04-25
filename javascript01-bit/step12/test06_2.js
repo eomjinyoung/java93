fbdd8565967dcab05988681f6102688ed3d7d6f1
eomjinyoung <jinyoung.eom@gmail.com>
2017-04-25 15:58:53 +0900

    .

--- javascript01-bit/step12/test06_2.js
+++ javascript01-bit/step12/test06_2.js
@@ -12,6 +12,11 @@
 
 const http = require('http')
 const url = require('url')
+/* Query String 분석기 준비
+=> name=value&name2=value2&... 형태의 문자열을 분석하여
+  값을 꺼내기 쉽도록 객체로 만들어 준다.
+*/
+const qs = require('querystring')
 
 const server = http.createServer(function(request, response) {
   var urlInfo = url.parse(request.url, true)
@@ -26,6 +31,8 @@ const server = http.createServer(function(request, response) {
   //2) POST 요청 데이터를 읽을 때 마다 'data' 이벤트가 발생하는데,
   //이 이벤트를 처리해야 한다.
   //즉 데이터를 읽을 때 마다, 그 읽은 데이터를 buf 변수에 저장시킨다.
+  //=> 'data' 이벤트는 클라이언트가 보낸 대량의 데이터 중에서 일부 데이터를 읽을 때 마다
+  //   발생된다. 이렇게 하는 이유는 개발자에게 처리 기회를 주기 위함이다.
   request.on('data', (data) => {
     buf += data
   })
@@ -36,17 +43,15 @@ const server = http.createServer(function(request, response) {
     response.writeHead(200, {
       'Content-Type' : 'text/plain;charset=UTF-8'
     })
-    response.write(buf)
+
+    //4) 쿼리스트링 분석기를 이용하여 문자열을 객체로 바꾼다.
+    var params = qs.parse(buf)
+
+    response.write('name=' + params.name + '\n')
+    response.write('age=' + params.age + '\n')
+    response.write('tel=' + params.tel + '\n')
     response.end()
   })
-
-
-  /*/
-  response.write('name=' + urlInfo.query.name + '\n')
-  response.write('age=' + urlInfo.query.age + '\n')
-  response.write('tel=' + urlInfo.query.tel + '\n')
-  response.end();
-  */
 })
 
 server.listen(8888)
