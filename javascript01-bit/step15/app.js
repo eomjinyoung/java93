257f42cd06b0cad26fdee1d9b6e15e721f8874fc
eomjinyoung <jinyoung.eom@gmail.com>
2017-04-26 17:37:13 +0900

    .

--- javascript01-bit/step15/app.js
+++ javascript01-bit/step15/app.js
@@ -6,14 +6,21 @@ var path = require('path')
 var cons = require('consolidate');
 
 var app = express()
+
+// 웹브라우저가 요청하는 파일이 어떤 폴더에 있는지 지정한다.
 app.use(express.static('public'))
+
+// POST 요청 데이터 분석기를 등록한다.
+// => 이 분석기를 등록해야만 POST 요청으로 들어온 데이터를 꺼낼 수 있다.
 app.use(bodyParser.urlencoded({extended: false}))
 
-app.engine('html', cons.handlebars)
-app.set('view engine', 'html')
-app.set('views', path.join(__dirname, '/views'))
+
+app.engine('html', cons.handlebars) // 사용할 템플릿 엔진을 등록한다.
+app.set('view engine', 'html') // 기본 템플릿 엔진으로 어떤 것을 사용할 지 지정한다.
+app.set('views', path.join(__dirname, '/views')) // 템플릿 파일이 있는 위치를 지정한다.
 
 // 서비스 라우터를 등록한다.
+// => http://localhost:8888/student/.... 요청을 처리할 라우터를 등록한다.
 app.use('/student', require('./control/student-control'))
 
 app.get('/', function(request, response) {
--- javascript01-bit/step15/control/student-control.js
+++ javascript01-bit/step15/control/student-control.js
@@ -2,12 +2,24 @@
 const express = require('express')
 const datasource = require('../util/datasource')
 const studentDao = require('../dao/student-dao')
+const memberDao = require('../dao/member-dao')
 const studentService = require('../service/student-service')
+
+const connection = datasource.getConnection()
+studentDao.setConnection(connection)
+memberDao.setConnection(connection)
+studentService.setStudentDao(studentDao)
+studentService.setMemberDao(memberDao)
 
 const router = express.Router()
 
-router.get('/main.do', (request, response) => {
-  response.render('student/index')
+router.get('/list.do', (request, response) => {
+  studentService.list(1, function(results, totalCount) {
+    response.render('student/index', {data: results})
+  }, function(error) {
+    response.end('오류!')
+    throw error
+  })
 })
 
 module.exports = router
