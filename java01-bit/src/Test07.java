37ac959b0041cf1bd309219e52bd9d12a5dfcbb2
eomjinyoung <jinyoung.eom@gmail.com>
2017-05-01 12:57:29 +0900

    .

---
+++ java01-bit/src/Test07.java
@@ -0,0 +1,31 @@
+/* 자바 프로그래밍 - 표준 출력 장치로 출력하기*/
+public class Test07 {
+  public static void main(String[] args) {
+    System.out.print("Hello,");
+    System.out.print("world!\n");
+    System.out.println("Hello,");
+    System.out.print("world!");
+  }
+}
+
+/*
+System 클래스
+- JVM의 기능이 들어 있다.
+
+out 객체
+- 표준 출력 장치에 대한 기능이 들어 있다.
+
+print()
+- 출력 기능을 수행하는 메서드(함수)
+
+println()
+- print() + 줄바꿈 기호 출력
+*/
+
+
+
+
+
+
+
+//
