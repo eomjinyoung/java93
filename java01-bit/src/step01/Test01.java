46e31ad05a9c947dabbb65bd770b8cc6ee7f60a1
eomjinyoung <jinyoung.eom@gmail.com>
2017-05-01 16:47:21 +0900

    .

---
+++ java01-bit/src/step01/Test01.java
@@ -0,0 +1,19 @@
+/* 주제: 이클립스에서 소스를 작성하고 컴파일하고 실행해보기!
+=> 패키지에 소속된 클래스 실행하기
+    java -cp bin 패키지명.패키지명.패키지명.클래스명
+  java01> java -cp bin step01.Test01
+=> 주의!
+  클래스가 있는 위치를 지정할 때 패키지명까지 지정해서는 안된다.
+  반드시 패키지가 있는 루트 폴더까지만 지정해야 한다.
+  java01> java -cp bin/step01 Test01
+ */
+package step01;
+
+public class Test01 {
+
+  public static void main(String[] args) {
+    System.out.println("Hello, world!");
+
+  }
+
+}
