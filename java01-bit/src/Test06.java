4b0c3fe0ad6704986f3e99c88c7ef09ccdfc02e3
eomjinyoung <jinyoung.eom@gmail.com>
2017-05-01 12:25:01 +0900

    .

---
+++ java01-bit/src/Test06.java
@@ -0,0 +1,28 @@
+/* 자바 프로그래밍 - 프로그램을 실행시킬 때 시작점(entry point)*/
+public class Test06 {
+  public static void main(String[] args) {
+    System.out.println("Hello!");
+  }
+}
+
+/*
+엔트리 포인트(entry point)
+- 프로그램을 실행시킬 때 가장 처음 실행하는 부분을 가리킨다.
+
+메서드(=함수)
+- 함수 중에서 객체의 값을 다루는 함수를 보통 '메서드(method)'라 부른다.
+- 자바에서는 모든 함수가 클래스 블록 안에 선언된다.
+- 함수(function)라는 용어보다 메서드(method)라는 용어를 사용한다.
+
+main() 메서드
+- JVM이 클래스 파일을 실행할 때 가장 먼저 실행하는 코드 블록이다.
+- main() 메서드가 없으면 해당 클래스를 JVM이 실행할 수 없다.
+*/
+
+
+
+
+
+
+
+//
