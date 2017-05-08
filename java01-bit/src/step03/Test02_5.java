983560c4d83aea8e72d27f569ad5484a6f4dfb78
eomjinyoung <jinyoung.eom@gmail.com>
2017-05-08 11:09:17 +0900

    .

--- java01-bit/src/step03/Test02_5.java
+++ java01-bit/src/step03/Test02_5.java
@@ -3,26 +3,28 @@
 package step03;
 
 public class Test02_5 {
+  
   public static void m1(int value) {
     System.out.println(value);
   }
 
   public static void main(String[] args) {
     int i = 3;
-    System.out.println(++i);
+    m1(++i);
     /* 실행 순서
      * => i = i + 1
-     * => System.out.println(4)
-     * => println() 실행!
+     * => m1(4) <-- i 변수의 값을 그 자리에 놓는다.
+     * => m1(4) <-- 함수를 호출한다.
      */
     
     i = 3;
-    System.out.println(i++);
+    m1(i++);
     /* 실행 순서
-     * System.out.println(3)  // 함수를 호출할 때 괄호() 안의 명령이 모두 실행돼야지만 
-     *                        // 함수를 호출할 수 있다.
-     * i = i + 1
-     * println() 실행!
+     * m1(3) <-- i 변수의 값을 그 자리에 놓는다. 
+     *           함수를 호출할 때 괄호() 안의 명령이 모두 실행돼야지만 
+     *           함수를 호출할 수 있다. 그래서 당장 호출하지는 않는다.
+     * i = i + 1 
+     * m1(3) <-- 함수를 호출한다.
      */
   }
 }
