2ef2b6b76b921f5a1e684af3ce2ef0f1e270d063
eomjinyoung <jinyoung.eom@gmail.com>
2017-05-15 10:20:43 +0900

    .

---
+++ java01-bit/src/step08/Test06_2.java
@@ -0,0 +1,31 @@
+/* 컬렉션(Collection) 클래스 : 사용 전 - 목록을 다루는 클래스 구현 
+ * => 배열을 이용하여 데이터 목록을 다루는 ArrayList 구현
+ * 
+ */
+package step08;
+
+import java.sql.Date;
+
+public class Test06_2 {
+  
+  public static void main(String[] args) {
+    Test06_2_ArrayList dateList = new Test06_2_ArrayList();
+    
+    dateList.add(Date.valueOf("2017-3-1"));
+    dateList.add(Date.valueOf("2017-5-3"));
+    dateList.add(Date.valueOf("2017-5-5"));
+    dateList.add(Date.valueOf("2017-5-9"));
+    dateList.add(Date.valueOf("2017-6-6"));
+    
+    for (int i = 0; i < dateList.size(); i++) {
+      System.out.println(dateList.get(i));
+    }
+  }
+}
+
+
+
+
+
+
+
---
+++ java01-bit/src/step08/Test06_2_ArrayList.java
@@ -0,0 +1,72 @@
+/* 목록을 다루는 클래스 구현 
+ * => 배열을 이용하여 데이터 목록을 다루는 ArrayList 구현
+ * 
+ */
+package step08;
+
+class Test06_2_ArrayList {
+  // 값을 조회하는 용도로만 사용하고 싶은 변수가 있다면, 읽기 전용으로 만들어라!
+  // => final 명령을 변수 선언 앞에 붙인다.
+  // => 조회용임을 직관적으로 알 수 있도록 보통 대문자로 변수명을 짓는다.
+  //    그리고 단어와 단어 사이에는 '_' 문자를 사용하여 구분한다. 
+  // => 이렇게 조회용으로만 사용하는 변수를 상수 변수라 한다. 보통 "상수"라고 줄여서 표현한다.
+  // => 상수는 인스턴스 마다 고유의 값을 가지지 않는다. 모든 인스턴스가 같은 값을 갖는다.
+  //    그래서 모든 인스턴스가 같은 값을 갖는 변수인 경우는 보통 클래스 변수로 선언한다.
+  //    클래스 변수로 선언하면 인스턴스마다 그 변수를 만들 필요가 없어 메모리가 절약된다.
+  // => 상수 변수 중에서 공개하는 변수인경우 public을 붙인다.
+  public static final int DEFAULT_SIZE = 3;
+  public static final int GROW_SIZE = 3;
+  
+  // 클래스 멤버만이 접근할 수 있게 접근을 제한한다.
+  private Object[] list;
+  private int length;
+  
+  public Test06_2_ArrayList() {
+    list = new Object[DEFAULT_SIZE];
+  }
+  
+  public Test06_2_ArrayList(int capacity) {
+    if (capacity < DEFAULT_SIZE || capacity > GROW_SIZE) {
+      list = new Object[DEFAULT_SIZE];
+    } else {
+      list = new Object[capacity];
+    }
+  }
+  
+  public void add(Object obj) {
+    list[length++] = obj;
+    
+    if (length  == list.length) {
+      increaseList();
+    }
+  }
+
+  // ArrayList 클래스 내부에서만 사용할 메서드라면 굳이 공개하지 말라!
+  // => 메서드 선언 앞에 private을 붙인다.
+  private void increaseList() {
+    Object[] newList = new Object[list.length + GROW_SIZE];
+    for (int i = 0; i < list.length; i++) {
+      newList[i] = list[i];
+    }
+    list = newList;
+  }
+  
+  public int size() {
+    return length;
+  }
+  
+  public Object get(int index) {
+    if (index < 0 || index >= length) {
+      throw new RuntimeException("배열 인덱스의 범위를 벗어났습니다.");
+    }
+    
+    return list[index];
+  }
+}
+
+
+
+
+
+
+
