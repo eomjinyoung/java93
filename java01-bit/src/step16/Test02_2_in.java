5ccc381299d49fcfdfd0724269ce64f31770cc8c
eomjinyoung <jinyoung.eom@gmail.com>
2017-05-19 10:56:49 +0900

    .

--- java01-bit/src/step16/Test02_2_in.java
+++ java01-bit/src/step16/Test02_2_in.java
@@ -1,27 +1,6 @@
-/* File I/O : 바이트 스트림 클래스 - FileInputStream
- * => 바이트 스트림(byte stream, binary stream)?
- *    중간에 변경없이 바이트 단위 그대로 읽고 쓰는 클래스들.
- * => 상속 계층 구조
- *    InputStream
- *      1) Data Sink Stream 클래스(데이터를 직접 읽고/쓰는 클래스)
- *         -> FileInputStream
- *         -> ByteArrayInputStream
- *         -> PipedInputStream
- *      2) Data Processing Stream 클래스(중간에서 데이터 가공)
- *         -> BufferedInputStream
- *         -> DataInputStream
- *         -> ObjectInputStream
- *         
- *    OutputStream
- *      1) Data Sink Stream 클래스(데이터를 직접 읽고/쓰는 클래스)
- *         -> FileOutputStream
- *         -> ByteArrayOutputStream
- *         -> PipedOutputStream
- *      2) Data Processing Stream 클래스(중간에서 데이터 가공)
- *         -> BufferedOutputStream
- *         -> DataOutputStream
- *         -> ObjectOutputStream
- *         -> PrintStream
+/* File I/O : 바이트 스트림 클래스 - FileInputStream II
+ * => 바이트 배열 읽기
+ * 
  */
 package step16;
 
--- java01-bit/src/step16/Test02_2_out.java
+++ java01-bit/src/step16/Test02_2_out.java
@@ -8,7 +8,7 @@ import java.io.FileOutputStream;
 public class Test02_2_out {
 
   public static void main(String[] args) throws Exception {
-    FileOutputStream out = new FileOutputStream("test02_3.data");
+    FileOutputStream out = new FileOutputStream("test02_2.data");
 
     byte[] bytes = {0x11, 0x22, 0x33, 0x44, 0x55, 0x66, 0x77};
     out.write(bytes); // 바이트 배열의 레퍼런스를 주면 그 배열의 값을 모두 출력한다.
