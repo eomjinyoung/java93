96e2d726e8720b1b423c6706d0f340e904164d4b
eomjinyoung <jinyoung.eom@gmail.com>
2017-05-23 12:31:57 +0900

    .

--- java01-bit/src/step18/Test02_1.java
+++ java01-bit/src/step18/Test02_1.java
@@ -1,6 +1,10 @@
 /* 소켓 프로그래밍: 서버 소켓 대기열(서버)
  * => 서버 소켓을 생성할 때 대기열의 최대 개수를 지정할 수 있다.
  *    그 개수 만큼만 클라이언트의 연결 요청을 허락한다.
+ *    
+ * => 대기열이 꽉 찬 상태에서 클라이언트가 요청할 때,
+ *    그 클라이언트는 timeout 시간 만큼 기다리다가 
+ *    그 때까지 대기열에 자리가 나지 않으면 즉시 예외를 던진다.
  */
 package step18;
 
@@ -19,8 +23,12 @@ public class Test02_1 {
     // => port: 포트번호
     // => backlog: 대기열의 크기
     ServerSocket serverSocket = new ServerSocket(8888, 3);
+    System.out.println("=> 서버 소켓 생성 완료!");
     
+    // 대기열에 기다리고 있는 클라이언트가 없다면, 리턴하지 않는다.
     Socket socket = serverSocket.accept();
+    System.out.println("=> 클라이언트 연결 승인!");
+    
     InputStream in0 = socket.getInputStream();
     OutputStream out0 = socket.getOutputStream();
     
--- java01-bit/src/step18/Test02_2.java
+++ java01-bit/src/step18/Test02_2.java
@@ -24,7 +24,7 @@ public class Test02_2 {
     
     // 3) 사용자로부터 입력 받은 값을 가지고 서버에 접속한다.
     Socket socket = new Socket(serverAddress, port);
-    System.out.println("소켓 객체 생성 완료!");
+    System.out.println("=> 소켓 객체 생성 완료!");
     
     // 4) 입출력할 스트림 객체 준비
     InputStream in0 = socket.getInputStream();
@@ -33,7 +33,7 @@ public class Test02_2 {
     // 5) 스트림 객체에 입출력 보조 객체를 연결한다.
     Scanner in = new Scanner(in0);
     PrintStream out = new PrintStream(out0);
-    System.out.println("입출력 스트림 준비 완료!");
+    System.out.println("=> 입출력 스트림 준비 완료!");
     
     // 6) 키보드에서 서버에 보낼 메시지를 입력 받는다.
     System.out.print("서버에 보낼 메시지: ");
@@ -41,10 +41,11 @@ public class Test02_2 {
     
     // 7) 키보드에서 입력 받은 메시지를 서버에 보낸다.
     out.println(message);
-    System.out.println("서버에 메시지 전송 완료!");
+    System.out.println("=> 서버에 메시지 전송 완료!");
     
     // 8) 서버가 보낸 메시지를 읽는다.
     String response = in.nextLine();
+    System.out.println("=> 서버로부터 메시지 수신 완료!");
     
     // 9) 서버가 보낸 메시지를 화면에 출력한다.
     System.out.println(response);
