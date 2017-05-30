4313160ac04a07d8001a643c1ea716746425900f
eomjinyoung <jinyoung.eom@gmail.com>
2017-05-30 16:08:37 +0900

    .

--- java01-bit/src/step22/ex2/MemberDao.java
+++ java01-bit/src/step22/ex2/MemberDao.java
@@ -1,5 +1,7 @@
 /* 역할:
  * => memb 테이블의 데이터를 다루는 메서드를 모아둔 클래스이다.
+ * => 출력하는 역할은 호출자에게 맡긴다.
+ * => DAO의 역할은 결과를 리턴하면 된다.
  */
 package step22.ex2;
 
@@ -8,6 +10,8 @@ import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
 
 public class MemberDao {
   Connection con;
@@ -18,70 +22,62 @@ public class MemberDao {
     con = DriverManager.getConnection(url, username, password); 
   }
   
-  public void selectList() {
+  public List<Member> selectList() throws Exception {
     try ( 
       Statement stmt = con.createStatement();
       ResultSet rs = stmt.executeQuery("select mno, name, tel, email from memb");) {
     
+      ArrayList<Member> list = new ArrayList<>();
+      Member member = null;
       while (rs.next()) { 
-        System.out.printf("%d, %s, %s, %s\n", 
-            rs.getInt("mno"), 
-            rs.getString("name"), 
-            rs.getString("tel"), 
-            rs.getString("email"));
-      }
+        member = new Member();
+        member.setNo(rs.getInt("mno"));
+        member.setName(rs.getString("name"));
+        member.setTel(rs.getString("tel"));
+        member.setEmail(rs.getString("email"));
         
-    } catch (Exception e) {
-      e.printStackTrace();
-    }
+        list.add(member);
+      }
+      return list; 
+    } /*catch (Exception e) { // 예외가 발생하면 단순히 호출자에게 보고할 것이기 때문에, 
+      throw e;                // 여기서는 catch 블록이 필요 없다.
+    }*/
   }
   
-  public void insert(String name, String tel, String email, String password) {
+  public int insert(Member member) throws Exception {
     try (
       PreparedStatement stmt = con.prepareStatement(
           "insert into memb(name,tel,email,pwd) values(?,?,?,password(?))");) {
       
-      stmt.setString(1, name);
-      stmt.setString(2, tel);
-      stmt.setString(3, email);
-      stmt.setString(4, password);
-      int count = stmt.executeUpdate();
-
-      System.out.println(count);
-    } catch (Exception e) {
-      e.printStackTrace();
+      stmt.setString(1, member.getName());
+      stmt.setString(2, member.getTel());
+      stmt.setString(3, member.getEmail());
+      stmt.setString(4, member.getPassword());
+      return stmt.executeUpdate();
     }
   }
   
-  public void delete(int no) {
+  public int delete(int no) throws Exception {
     try (
       PreparedStatement stmt = con.prepareStatement(
           "delete from memb where mno=?");) {
       
       stmt.setInt(1, no);
-      int count = stmt.executeUpdate();
-      System.out.println(count);
-      
-    } catch (Exception e) {
-      e.printStackTrace();
+      return stmt.executeUpdate();
     }
   }
   
-  public void update(int no, String name, String tel, String email, String password) {
+  public int update(Member member) throws Exception {
     try (
       PreparedStatement stmt = con.prepareStatement(
           "update memb set name=?, tel=?, email=?, pwd=password(?) where mno=?");) {
       
-      stmt.setString(1, name);
-      stmt.setString(2, tel);
-      stmt.setString(3, email);
-      stmt.setString(4, password);
-      stmt.setInt(5, no);
-      int count = stmt.executeUpdate();
-      System.out.println(count);
-      
-    } catch (Exception e) {
-      e.printStackTrace();
+      stmt.setString(1, member.getName());
+      stmt.setString(2, member.getTel());
+      stmt.setString(3, member.getEmail());
+      stmt.setString(4, member.getPassword());
+      stmt.setInt(5, member.getNo());
+      return stmt.executeUpdate();
     }
   }
   
--- java01-bit/src/step22/ex2/Test01_1.java
+++ java01-bit/src/step22/ex2/Test01_1.java
@@ -1,18 +1,13 @@
-/* JDBC 프로그래밍 II: DAO(Data Access Object) 적용
- * => DAO? 데이터를 저장, 조회, 변경, 삭제를 처리하는 
- *    그런 역할을 수행하는 객체를 부르는 말이다.
- * => 보통 테이블 당 한 개의 DAO를 만든다.
- * => 물론 SELECT 할 때 조인을 수행하는 경우,
- *    여러 테이블의 데이터를 다루기도 한다.
- * => 이렇게 데이터를 다루는 코드를 별도의 클래스로 분류해 놓으면
- *    소스 코드를 유지보수하기 편하다.
+/* JDBC 프로그래밍 II: DAO에서 출력 기능 제거, 데이터 처리만 하도록 변경
+ * => memb 테이블의 값을 저장할 새로운 데이터 타입을 정의한다.
  * => 작업
- *    1) memb 테이블의 등록, 조회, 변경, 삭제를 다루는 DAO 클래스를 만든다.
- *       예) MemberDao
- *    2) MemberDao 클래스에 등록, 조회, 변경, 삭제를 수행하는 메서드를 정의한다.
- *    3) Test06_x 에서 MemberDao를 사용하여 멤버 데이터를 다룬다.
+ *    1) Member 클래스 정의
+ *    2) MemberDao 클래스를 변경
+ *    3) MemberDao 사용
  */
 package step22.ex2;
+
+import java.util.List;
 
 public class Test01_1 {
 
@@ -23,37 +18,41 @@ public class Test01_1 {
     String jdbcPassword = "1111";
     
     MemberDao memberDao = new MemberDao(jdbcDriver, jdbcUrl, jdbcUsername, jdbcPassword);
-    memberDao.selectList();
+    List<Member> list = memberDao.selectList();
+    for (Member m : list) {
+      System.out.printf("%d, %s, %s, %s\n", m.getNo(), m.getName(), m.getTel(), m.getEmail());
+    }
     System.out.println("-----------------------------------------");
     
     /*
-    memberDao.delete(59);
-    memberDao.selectList();
+    System.out.println(memberDao.delete(59));
+    System.out.println("-----------------------------------------");
+    
+    Member member = new Member();
+    member.setName("홍길동104");
+    member.setTel("1111-1111");
+    member.setEmail("hong104@test.com");
+    member.setPassword("1111");
+ 
+    System.out.println(memberDao.insert(member));
     System.out.println("-----------------------------------------");
     */
+   
+    Member member = new Member();
+    member.setNo(60); // 변경하려는 데이터의 mno이어야 한다.
+    member.setName("홍길동 변경");
+    member.setTel("1111-2221");
+    member.setEmail("hong105@test.com");
+    member.setPassword("1111");
     
-    /*
-    memberDao.insert("홍길동104", "hong104@test.com", "1111-1111", "1111");
-    memberDao.selectList();
+    memberDao.update(member);
     System.out.println("-----------------------------------------");
-    */
     
-    /*
-    memberDao.update(59, "홍길동변경", "hong104@test.com", "1111-2222", "1313");
-    memberDao.selectList();
+    list = memberDao.selectList();
+    for (Member m : list) {
+      System.out.printf("%d, %s, %s, %s\n", m.getNo(), m.getName(), m.getTel(), m.getEmail());
+    }
     System.out.println("-----------------------------------------");
-    */
-    
-    /* 이 예제의 문제점?
-     * => DAO가 데이터를 처리하는 일 뿐만 아니라 화면에 출력하는 일도 담당한다.
-     * => 만약 이 DAO를 윈도우 프로그램을 개발할 때 쓰려한다면,
-     *    안타깝지만 재사용할 수 없다.
-     *    
-     * "High Cohesion"
-     * => 한 클래스는 한 가지 역할만 해야지 재사용성이 좋아진다.
-     * => 여러 가지 역할을 하면 재사용하기 힘들다.
-     */
-    
   }
 }
 
