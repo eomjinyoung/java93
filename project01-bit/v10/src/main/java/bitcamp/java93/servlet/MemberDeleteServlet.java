7c036369b0f71edbcd3f05bea6ff1d344b78c556
eomjinyoung <jinyoung.eom@gmail.com>
2017-06-09 11:40:58 +0900

    .

---
+++ project01-bit/v10/src/main/java/bitcamp/java93/servlet/MemberDeleteServlet.java
@@ -0,0 +1,75 @@
+package bitcamp.java93.servlet;
+/* ServletContext 보관소에 저장된 MemberDao 이용하기 
+ */
+
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import bitcamp.java93.dao.MemberDao;
+
+@WebServlet(urlPatterns="/member/delete") 
+public class MemberDeleteServlet  extends HttpServlet {
+  private static final long serialVersionUID = 1L;
+
+  @Override
+  public void service(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
+    res.setContentType("text/html;charset=UTF-8");
+    PrintWriter out = res.getWriter();
+    
+    out.println("<!DOCTYPE html>");
+    out.println("<html>");
+    out.println("<head>");
+    out.println("  <meta charset='UTF-8'>");
+    out.println("  <title>회원관리</title>");
+    
+    // including 기법을 사용하여 각 페이지에 기본 CSS 스타일 코드를 출력한다.
+    RequestDispatcher rd = req.getRequestDispatcher("/style/core");
+    rd.include(req, res);
+        
+    out.println("</head>");
+    out.println("<body>");
+    out.println("<h1>회원 삭제</h1>");
+    
+    try {
+      MemberDao memberDao = (MemberDao)this.getServletContext().getAttribute("memberDao");      
+      int no = Integer.parseInt(req.getParameter("no"));
+      
+      int count = memberDao.delete(no);
+      if (count < 1) {
+        throw new Exception(no + "번 회원을 찾을 수 없습니다.");
+      }
+      out.println("<p>삭제 성공입니다.</p>");
+      res.setHeader("Refresh", "1;url=list");
+      
+    } catch (Exception e) {
+      req.setAttribute("error", e); // ServletRequest 보관소에 오류 정보를 보관한다.
+      rd = req.getRequestDispatcher("/error");
+      rd.forward(req, res);
+      return;
+    }
+    
+    // including 기법을 사용하여 각 페이지마다 꼬리말을 붙인다.
+    rd = req.getRequestDispatcher("/footer");
+    rd.include(req, res);
+    
+    out.println("</body>");
+    out.println("</html>");
+  }
+}
+
+
+
+
+
+
+
+
+
