1bc91f600a60781ebeff85085a30b1a65905855a
eomjinyoung <jinyoung.eom@gmail.com>
2017-06-23 10:08:16 +0900

    .

---
+++ java01-bit/src/step28/ex4/ProjectDao.java
@@ -0,0 +1,5 @@
+package step28.ex4;
+
+public interface ProjectDao {
+  Project selectOne(int no) throws Exception;
+}
---
+++ java01-bit/src/step28/ex4/ProjectDaoImpl.java
@@ -0,0 +1,25 @@
+package step28.ex4;
+
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+
+public class ProjectDaoImpl implements ProjectDao {
+  SqlSessionFactory sqlSessionFactory;
+  
+  public void setSqlSessionFactory(SqlSessionFactory sqlSessionFactory) {
+    this.sqlSessionFactory = sqlSessionFactory;
+  }
+
+  public Project selectOne(int no) throws Exception {
+    SqlSession sqlSession = sqlSessionFactory.openSession();
+    
+    try {
+      return sqlSession.selectOne("step28.ex4.ProejectDao.selectOne", no);
+      
+    } finally {
+      sqlSession.close();
+    }
+  }
+
+  
+}
