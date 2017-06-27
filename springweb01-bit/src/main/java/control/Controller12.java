aa740bf41c8df63d9cec585a4466229397dd6e9e
eomjinyoung <jinyoung.eom@gmail.com>
2017-06-27 10:00:03 +0900

    .

--- springweb01-bit/src/main/java/control/Controller12.java
+++ springweb01-bit/src/main/java/control/Controller12.java
@@ -16,7 +16,9 @@ import java.io.PrintWriter;
 import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResultUtils;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @RequestMapping("/control/controller12/") 
@@ -29,12 +31,72 @@ public class Controller12 {
   public void ok1(String name, int age, boolean working, HttpServletResponse response) throws Exception {
     response.setContentType("text/plain;charset=UTF-8");
     PrintWriter out = response.getWriter();
-    out.println("ok10()");
+    out.println("ok1()");
     out.printf("name: %s\n", name);
     out.printf("age: %d\n", age);
     out.printf("working: %b\n", working);
   }
   
+  // 2) 보내는 데이터(요청 파라미터)의 이름과 아규먼트 이름이 다를 경우
+  //    @RequestParam 애노테이션을 사용하여 그 이름을 지정할 수 있다.
+  //    단, @RequestParam을 붙이는 순간 필수 항목이 된다.
+  @RequestMapping("ok2")
+  public void ok2(
+      @RequestParam(name="name") String n, 
+      @RequestParam(name="age") int a, 
+      @RequestParam(name="working") boolean w, 
+      HttpServletResponse response) throws Exception {
+    response.setContentType("text/plain;charset=UTF-8");
+    PrintWriter out = response.getWriter();
+    out.println("ok2()");
+    out.printf("name: %s\n", n);
+    out.printf("age: %d\n", a);
+    out.printf("working: %b\n", w);
+  }
+  
+  // 3) @RequestParam 애노테이션에서 요청 파라미터 값을 선택 항목으로 만들기
+  @RequestMapping("ok3")
+  public void ok3(
+      @RequestParam(name="name", required=false) String n, 
+      @RequestParam(name="age", required=false) int a, 
+      @RequestParam(name="working", required=false) boolean w, 
+      HttpServletResponse response) throws Exception {
+    response.setContentType("text/plain;charset=UTF-8");
+    PrintWriter out = response.getWriter();
+    out.println("ok3()");
+    out.printf("name: %s\n", n);
+    out.printf("age: %d\n", a);
+    out.printf("working: %b\n", w);
+  }  
+
+  // 4) @RequestParam 애노테이션에서 요청 파라미터의 기본 값 지정하기
+  @RequestMapping("ok4")
+  public void ok4(
+      @RequestParam(name="name", required=false) String n, 
+      @RequestParam(name="age", required=false, defaultValue="0") int a, 
+      @RequestParam(name="working", required=false, defaultValue="false") boolean w, 
+      HttpServletResponse response) throws Exception {
+    response.setContentType("text/plain;charset=UTF-8");
+    PrintWriter out = response.getWriter();
+    out.println("ok4()");
+    out.printf("name: %s\n", n);
+    out.printf("age: %d\n", a);
+    out.printf("working: %b\n", w);
+  }
+  
+  // 5) 요청 파라미터 값을 바로 값 객체(Value Object; VO)에 저장하기  
+  //    VO 객체의 프로퍼티 이름과 일치하는 요청 파라미터를 찾아서 그 값을 넣어준다.
+  //    누가? 프론트 컨트롤러가 Member 객체를 생성한 다음에 값을 넣어준다.
+  //    
+  @RequestMapping("ok5")
+  public void ok5(Member member, HttpServletResponse response) throws Exception {
+    response.setContentType("text/plain;charset=UTF-8");
+    PrintWriter out = response.getWriter();
+    out.println("ok5()");
+    out.printf("name: %s\n", member.getName());
+    out.printf("age: %d\n", member.getAge());
+    out.printf("working: %b\n", member.isWorking());
+  }  
 }
 
 
---
+++ springweb01-bit/src/main/java/control/Member.java
@@ -0,0 +1,28 @@
+package control;
+
+public class Member {
+  String name;
+  int age;
+  boolean working;
+  
+  public String getName() {
+    return name;
+  }
+  public void setName(String name) {
+    this.name = name;
+  }
+  public int getAge() {
+    return age;
+  }
+  public void setAge(int age) {
+    this.age = age;
+  }
+  public boolean isWorking() {
+    return working;
+  }
+  public void setWorking(boolean working) {
+    this.working = working;
+  }
+  
+  
+}
