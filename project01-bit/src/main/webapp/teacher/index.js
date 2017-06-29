c2b8d152432ecde7e0c64d6cea921466273a45d2
eomjinyoung <jinyoung.eom@gmail.com>
2017-06-29 17:09:56 +0900

    .

--- project01-bit/src/main/webapp/teacher/index.js
+++ project01-bit/src/main/webapp/teacher/index.js
@@ -18,15 +18,12 @@ nextBtn.click(function() {
 function displayList(pageNo) {
   // 서버에서 강사 목록 데이터를 받아 온다.
   $.getJSON('list.json', {'pageNo':pageNo, 'pageSize': pageSize}, function(result) {
-	console.log(result);
-	var list = result.data;
-	/*
-	var totalCount = result.totalCount;
+	var totalCount = result.data.totalCount;
     var lastPageNo = parseInt(totalCount / pageSize) + (totalCount % pageSize > 0 ? 1 : 0)
 
     // 템플릿 소스를 가지고 템플릿을 처리할 함수를 얻는다.
     var templateFn = Handlebars.compile($('#tbody-template').text())
-    var generatedHTML = templateFn(result) // 템플릿 함수에 데이터를 넣고 HTML을 생성한다.
+    var generatedHTML = templateFn(result.data) // 템플릿 함수에 데이터를 넣고 HTML을 생성한다.
     tbody.text('') // tbody의 기존 tr 태그들을 지우고
     tbody.html(generatedHTML) // 새 tr 태그들로 설정한다.
 
@@ -44,7 +41,6 @@ function displayList(pageNo) {
     } else {
       nextBtn.prop('disabled', false)
     }
-    */
   }) // getJSON()
 } // displayList()
 
